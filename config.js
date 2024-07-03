4b11be68a450df9ac3d79f1ae723345259948db9
alexandruxtrifon <alexandruxtrifon@gmail.com>
2024-07-03 16:17:58 +0300

    get si post functioneaza
    Signed-off-by: alexandruxtrifon <alexandruxtrifon@gmail.com>

---
+++ config.js
@@ -0,0 +1,12 @@
+const config = {
+    server: 'DATASRV\\SQL2019',
+    user: 'user_internship2024',
+    password: 'SarmalutaUcigasa3',
+    instancename: 'SQLEXPRESS',
+    options: {
+        database: 'internship2024',
+        trustServerCertificate: true
+    }
+};
+
+module.exports = config;
