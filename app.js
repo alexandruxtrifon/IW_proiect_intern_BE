a169915fcf150869af2e52e689dceba325fda737
alexandruxtrifon <alexandruxtrifon@gmail.com>
2024-07-04 10:39:04 +0300

    ping

--- app.js
+++ app.js
@@ -68,7 +68,7 @@ app.get('/api/test', async (req, res) => {
       });
     }
   });
-
+//
 const PORT = process.env.PORT || 3000;
 app.listen(PORT,console.log(
     `Server started on port ${PORT}`));
